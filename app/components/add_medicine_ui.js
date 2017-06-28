5788d54010bc63dd2488d4781ed649b47e0520b1
Nathan Mundo <nathan@Nathans-iMac.local>
2017-06-27 23:01:44 -0400

    React Router work

--- app/components/add_medicine_ui.js
+++ app/components/add_medicine_ui.js
@@ -2,8 +2,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 import {Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import moment from 'moment';
 import TimePicker from 'rc-time-picker';
+
+const ButtonToNavigate = ({ title, history }) => (
+  <button
+    type="button"
+    onClick={() => history.push('/')}
+  >
+    {title}
+  </button>
+);
 
 export default class MedicineListItem extends React.Component {
     render() {
@@ -21,9 +31,9 @@ export default class MedicineListItem extends React.Component {
                         onChange={this.onChange}
                         format={'h:mm a'}
                         use12Hours
-                    />
+                    /><br/>
+                    <Route path="/" render={(props) => <ButtonToNavigate {...props} title="Done" />} /><Link to='/'>Cancel</Link>
                 </form>
-                <Link to='/'>Cancel</Link>
             </div>
         );
     }
